cca2ba8d2735ac1de5796ff0476b9b80610b5eeb
9822951 <javier.perea@stucom.com>
2023-05-31 09:14:30 +0200

    Clase libro añadida con atributos (Getters y Setters añadidos)

---
+++ Libro.php
@@ -0,0 +1,68 @@
+<?php
+
+class Libro{
+    private string $isbn;
+    private string $titulo;
+    private string $categoria;
+
+
+    /**
+     * Get the value of isbn
+     */ 
+    public function getIsbn()
+    {
+        return $this->isbn;
+    }
+
+    /**
+     * Set the value of isbn
+     *
+     * @return  self
+     */ 
+    public function setIsbn($isbn)
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of titulo
+     */ 
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * Set the value of titulo
+     *
+     * @return  self
+     */ 
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of categoria
+     */ 
+    public function getCategoria()
+    {
+        return $this->categoria;
+    }
+
+    /**
+     * Set the value of categoria
+     *
+     * @return  self
+     */ 
+    public function setCategoria($categoria)
+    {
+        $this->categoria = $categoria;
+
+        return $this;
+    }
+}
