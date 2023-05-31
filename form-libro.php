1e73e5e2de97f63fac70ad68ef7a1c715b496ada
9822951 <javier.perea@stucom.com>
2023-05-31 09:44:46 +0200

    Formulario de libro añadido (MUY BÁSICO)

---
+++ form-libro.php
@@ -0,0 +1,11 @@
+
+<html>
+    <body>
+        <form method="POST" action="">
+            <input type="text" name="isbn" placeholder="Inserte isbn..."><br>
+            <input type="text" name="titulo" placeholder="Inserte titulo..."><br>
+            <input type="text" name="cat" placeholder="Inserte categoria..."><br>
+            <input type="submit">
+        </form>
+    </body>
+</html>
